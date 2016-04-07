63f7fb4b52495607b05183dfbb1da75c82ce0256
Aaron Ribant <aribant@gmail.com>
2016-04-07 15:02:05 -0400

    First commit of JavaScript basic data type examples

---
+++ DataTypes.js
@@ -0,0 +1,44 @@
+/*
+ > Greater than
+ < Less than
+ <= Less than or equal to
+ >= Greater than or equal to
+ === Equal to
+ !== Not equal to
+ */
+
+/*
+ Confirm and prompt
+
+ We can make pop-up boxes appear!
+ confirm("I am ok");
+ prompt("Annie are you ok?");
+
+ Data types
+
+ a. numbers (e.g. 4.3, 134)
+
+ b. strings (e.g. "dogs bark!", "JavaScript newbie")
+
+ c. booleans (e.g. false, 5 > 4)
+
+ Conditionals
+
+ If the first condition is met, execute the first code block. If it is not met, execute the code in the else block. See the code on the right for another example.
+ */
+
+
+
+// variable name is assigned the value of Aaron
+var name = "Aaron";
+
+// print to console the value of name
+console.log(name);
+
+// Length counts every character in the string - including spaces.
+// print to the console the length of name's value
+console.log(name.length);
+
+// Boolean example: 15 > 4 evaluates to true, so true is printed.
+console.log(15 > 4);
+
