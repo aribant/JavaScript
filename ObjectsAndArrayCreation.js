d875c5819d0d0fddf4d7566c7afb453d343f5bc5
Aaron Ribant <aribant@gmail.com>
2016-04-07 12:25:16 -0400

    First commit. JavaScript homework from CodeCademy Addressbook-Objects and an Array used

---
+++ ObjectsAndArrayCreation.js
@@ -0,0 +1,25 @@
+/**
+ * Instructions Create an object called mary. It has the same properties as bob.
+ * Her name is Mary Johnson, her phoneNumber is "(650) 888 - 8888" and her email
+ * is "mary.johnson@example.com". Create an array called contacts. Put bob in
+ * first (at index 0), then mary (at index 1). Write a console.log statement
+ * that prints out Mary's phone number.
+ */
+
+var bob = {
+	firstName : "Bob",
+	lastName : "Jones",
+	phoneNumber : "(650) 777-7777",
+	email : "bob.jones@example.com"
+};
+
+var mary = {
+	firstname : "Mary",
+	lastname : "Johnson",
+	phoneNumber : "(650) 888 - 8888",
+	email : "mary.johnson@example.com"
+};
+
+var contacts = [ bob, mary ];
+
+console.log(mary.phoneNumber);
