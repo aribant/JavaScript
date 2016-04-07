f7526641ab95f6b7778e6ac76c71f7309a0adb5b
Aaron Ribant <aribant@gmail.com>
2016-04-07 15:40:33 -0400

    Initial commit of AdditionViaPrompt - program that adds numbers entered via web prompt until 0 is entered. The sum is then displayed.

---
+++ AdditionViaPrompt.js
@@ -0,0 +1,13 @@
+/**
+ * Addition via web prompt
+ */
+<script>
+var sum = 0
+var num
+num = prompt("Enter new value, or 0 to end")
+while (num != 0) {
+    sum = sum + parseInt(num)
+    num = prompt("Enter new value, or 0 to end")
+}
+alert("Sum = " + sum)
+</script>
