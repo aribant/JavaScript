c97f0f46a99de0169928bfdf67495d1c8dcaee32
Aaron Ribant <aribant@gmail.com>
2016-04-07 16:05:57 -0400

    Example of using Modulo to print the remainder of a number

---
+++ ModuloExample.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Aaron
+ */
+// Below is an example of printing the remainder using modulo:
+
+console.log(14 % 3); // remainder of 2
+console.log(99%8); // remainder of 3
+console.log(11 % 3); // remainder of 2
