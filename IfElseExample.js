c97e252aeb43c18b745ae98961214edb5fcb3bb8
Aaron Ribant <aribant@gmail.com>
2016-04-07 15:48:42 -0400

    first commit

---
+++ IfElseExample.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Aaron
+ *
+ */
+
+// This is an example of an if / else statement.
+
+if (12 / 6 === "Aaron".length) {
+    console.log("true");
+} else {
+    console.log("false");
+}
