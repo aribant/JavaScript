bc5c1ee056f1b35c81255652ca206f35da26d657
Aaron Ribant <aribant@gmail.com>
2016-04-07 17:04:02 -0400

    Working with Substrings

---
+++ MoreSubstringPractice.js
@@ -0,0 +1,13 @@
+/**
+ * Using console.log, on three separate lines, print out the substrings for the following strings.
+
+ a. "Jan" in "January"
+ b. "Melbourne is" in "Melbourne is great" (note the space!)
+ c. "burgers" in "Hamburgers"
+ */
+// Use console.log( ) to print out the substrings.
+// Here is an example of the 1st to 4th letter of "JavaScript":
+// console.log("JavaScript".substring(0,4));
+console.log("January".substring(0,3));
+console.log("Melbourne is great".substring(0,12));
+console.log("Hamburgers".substring(3,10));
